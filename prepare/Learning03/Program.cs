001a1fb540c1fc1c48a04c90be1d6c9d7265df0e
easton2005 <ewri2005@byui.edu>
2025-09-25 15:41:23 -0600

    prep3change

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int userGuess = 0;
+        while (userGuess != magicNumber)
+            Console.Write("What is your guess? ");
+        userGuess = int.Parse(Console.ReadLine());
+        if (userGuess < magicNumber)
+            Console.WriteLine("Too low!");
+        else if (userGuess > magicNumber)
+            Console.WriteLine("Too high!");
+        else
+            Console.WriteLine("You guessed it!");
     }
 }
