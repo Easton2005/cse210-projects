083a8c0aa52d1d47faf8c2bcae367a547f819020
easton2005 <ewri2005@byui.edu>
2025-09-25 15:44:31 -0600

    changes

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,16 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        int magicNumber = randomGenerator.Next(1, 101); // include 100
         int userGuess = 0;
+
         while (userGuess != magicNumber)
+        {
             Console.Write("What is your guess? ");
-        userGuess = int.Parse(Console.ReadLine());
-        if (userGuess < magicNumber)
-            Console.WriteLine("Too low!");
-        else if (userGuess > magicNumber)
-            Console.WriteLine("Too high!");
-        else
-            Console.WriteLine("You guessed it!");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
