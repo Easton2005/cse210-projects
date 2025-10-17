11487f1081e9e63000aaee46dffdea86867fe077
easton2005 <ewri2005@byui.edu>
2025-10-16 18:31:57 -0600

    Fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,27 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Make some fraction objects
-        Fraction f1 = new Fraction();
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine("Enter the numerator (top number): ");
+        int top = int.Parse(Console.ReadLine());
 
-        Fraction f2 = new Fraction(5);
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine("Enter the denominator (bottom number): ");
+        int bottom = int.Parse(Console.ReadLine());
 
-        Fraction f3 = new Fraction(3, 4);
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
+        // Create a new fraction using what the user typed
+        Fraction userFraction = new Fraction(top, bottom);
 
-        Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
-
-        // Change values using setters
-        f4.SetTop(2);
-        f4.SetBottom(5);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
+        Console.WriteLine("Your fraction is: " + userFraction.GetFractionString());
+        Console.WriteLine("The decimal value is: " + userFraction.GetDecimalValue());
     }
 }
